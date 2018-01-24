fb80e94ffd0eb014333a6c93c454084f77b127d1
rpatriciocarvalho <rpatriciocarvalho@gmail.com>
2018-01-24 02:45:32 -0200

    Agora o mpi salva em um arquivo

--- main.c
+++ main.c
@@ -24,15 +24,12 @@ int main(int argc, char** argv){
     double desvio_magnetizacao, desvio_energia;
     double cumulante;
     char nome_arquivo[25];
+    int tamanho_linha = 250;
+    char linha[tamanho_linha];
 
     // Variáveis para computação paralela
     double m_temp_mpi;
     double inicio_node, fim_node, temperatura_node;
-    double magnetizacao_node, energia_node, magnetizacao_2_node;
-    double magnetizacao_4_node, energia_2_node;
-    double calor_especifico_node, suscetibilidade_mag_node, desvio_magnetizacao_node;
-    double desvio_energia_node, cumulante_node;
-
 
     time_t inicio; // Variáveis para marcar o tempo de execução
 	time_t fim;
@@ -71,16 +68,26 @@ int main(int argc, char** argv){
             MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
             MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
             
-            m_temp_mpi = (double) TEMP_F/(nprocs - 1); // Divide a temperatura por unidade de processamento
+            MPI_Offset offset;
+            MPI_File   file;
+            MPI_Status status;
+
+            // Cria um arquivo para armazenar os dados   
+            sprintf(nome_arquivo,"dados_%dx%dx%d_%u_[%d]_[%d-%d-%d].dat", NX, NY, NZ, N_PASSOS, 
+                    quantidade_medidas, VIZINHO_X, VIZINHO_Y, VIZINHO_Z);
+
+            MPI_File_open(MPI_COMM_WORLD, nome_arquivo, MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
+            
+            m_temp_mpi = (double) TEMP_F/nprocs; // Divide a temperatura por unidade de processamento
 
             // Estabelece temperaturas iniciais e finais para cada unidade de processamento
-            for(i=1; i<nprocs; i++){
+            for(i=0; i<nprocs; i++){
                 if(myrank == i){
-                    if (myrank == 1){ 
+                    if (myrank == 0){ 
                         inicio_node = INCRE_TEMP;
                         fim_node = m_temp_mpi;
                     } else {
-                        inicio_node = (i-1)*m_temp_mpi;
+                        inicio_node = m_temp_mpi + INCRE_TEMP;
                         fim_node = inicio_node + m_temp_mpi;
                     }                    
                 }
@@ -91,63 +98,34 @@ int main(int argc, char** argv){
             
             // Fator de normalização que será usado nas médias
             fator_normalizacao = (unsigned long int) (N_PASSOS - N_PASSOS*0.1)*NX*NY*NZ;
-
-            // Cria um arquivo para armazenar os dados    
-            FILE *arquivo_dados;
-                        
-            sprintf(nome_arquivo,"dados_%dx%dx%d_%u_[%d]_[%d-%d-%d].dat", NX, NY, NZ, N_PASSOS, 
-                quantidade_medidas, VIZINHO_X, VIZINHO_Y, VIZINHO_Z);
-
-            if(myrank == 0) {
-                arquivo_dados = fopen(nome_arquivo, "w"); // O arquivo é criado apenas no master
-                temperatura = 0;
-            }
             
-            // Zerando variáveis
-            magnetizacao = energia = 0.0;
-            magnetizacao_2 = magnetizacao_4 = energia_2 = 0.0;
-
-            if(myrank != 0) {
-                // É calculado as medidas para cada temperatura
-                for(temperatura_node=inicio_node;temperatura_node<=fim_node; temperatura_node+=INCRE_TEMP){
+            // É calculado as medidas para cada temperatura
+            for(temperatura_node=inicio_node;temperatura_node<=fim_node; temperatura_node+=INCRE_TEMP){
+            
+                iniciar_rede(); 
                 
-                    iniciar_rede(); 
+                // Zerando variáveis
+                magnetizacao = energia = 0.0;
+                magnetizacao_2 = magnetizacao_4 = energia_2 = 0.0;
+                
+                // Para cada unidade de processamento os passos de Monte Carlo
+                for(i=1; i <= N_PASSOS; i++){
                     
-                    // Zerando variáveis
-                    magnetizacao_node = energia_node = magnetizacao_2_node = 0.0;
-                    magnetizacao_4_node = energia_2_node = 0.0;
+                    metropolis(temperatura_node); // Algoritmo de Metropolis
                     
-                    // Para cada unidade de processamento os passos de Monte Carlo
-                    for(i=1; i <= N_PASSOS; i++){
-                        
-                        metropolis(temperatura_node); // Algoritmo de Metropolis
-                        
-                        // Começa a medir apenas depois da termalização
-                        if (i > passos_termalizacao){
-                            mag = fabsf(calcula_magnetizacao());
-                            ene = fabsf(calcula_energia());
-                            magnetizacao_node += mag;
-                            energia_node += ene;
-                            // Variáveis para cálculo dos desvios e outras grandezas
-                            magnetizacao_2_node += pow(mag, 2);
-                            magnetizacao_4_node += pow(mag, 4);
-                            energia_2_node += pow(ene, 2);                        
-                        }                    
-                    }
-
-                    // Envia os dados de cada unidade de processamento para a unidade de processamento primária
-                    MPI_Gather(&temperatura_node, 1, MPI_DOUBLE, &temperatura, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-                    MPI_Gather(&magnetizacao_node, 1, MPI_DOUBLE, &magnetizacao, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-                    MPI_Gather(&energia_node, 1, MPI_DOUBLE, &energia, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-                    MPI_Gather(&magnetizacao_2_node, 1, MPI_DOUBLE, &magnetizacao_2, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-                    MPI_Gather(&magnetizacao_4_node, 1, MPI_DOUBLE, &magnetizacao_4, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-                    MPI_Gather(&energia_2_node, 1, MPI_DOUBLE, &energia_2, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+                    // Começa a medir apenas depois da termalização
+                    if (i > passos_termalizacao){
+                        mag = fabsf(calcula_magnetizacao());
+                        ene = fabsf(calcula_energia());
+                        magnetizacao += mag;
+                        energia += ene;
+                        // Variáveis para cálculo dos desvios e outras grandezas
+                        magnetizacao_2 += pow(mag, 2);
+                        magnetizacao_4 += pow(mag, 4);
+                        energia_2 += pow(ene, 2);                        
+                    }                    
                 }
-            }
-            
-            // Se o processo estiver rodando na unidade de processamento primária, calcula-se as médias
-            if (myrank == 0) {
-                    
+
                 magnetizacao /= (double) fator_normalizacao;
                 energia /= (double) fator_normalizacao;
                 magnetizacao_2 /= (double) fator_normalizacao*NX*NY*NZ;
@@ -169,20 +147,21 @@ int main(int argc, char** argv){
                         energia média - desvio energia - calor específico 
                         suscetibilidade magnética - cumulante de Binder
                 */
-
                 
-                printf("%2.2f%%\n", (float) temperatura); // Porcentagem da simulacao
-                fprintf(arquivo_dados, "%f %2.20f %2.20f %2.20f %2.20f %2.20f %2.20f %2.20f\n", temperatura,
+                sprintf(linha, "%f %2.20f %2.20f %2.20f %2.20f %2.20f %2.20f %2.20f\n", temperatura,
                                     magnetizacao,
                                     desvio_magnetizacao,
                                     energia,
                                     desvio_energia,
                                     calor_especifico,
                                     suscetibilidade_mag,
-                                    cumulante);                
+                                    cumulante);   
+
+                MPI_File_write_all(file, linha, tamanho_linha, MPI_CHAR, &status)
+
             }
-        
-            if(myrank == 0) fclose(arquivo_dados); // Fechamos o arquivo
+            
+            MPI_File_close(&file);
             MPI_Finalize(); // Finalizamos o MPI        
         }
     }
